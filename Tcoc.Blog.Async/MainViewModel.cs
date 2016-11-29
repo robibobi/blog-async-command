7aa91c42aeadedde8590606977147ad820b7921c
tobias höfer <robibobi@gmx.de>
2016-11-29 07:00:56 +0100

    Add project files.

---
+++ Tcoc.Blog.Async/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tcoc.Blog.Async.Command;
+
+namespace Tcoc.Blog.Async
+{
+    class MainViewModel
+    {
+
+        private AsyncCommand<double> _calculatePrimesCmd;
+
+        public AsyncCommand<double> CalculatePrimesCmd => _calculatePrimesCmd; 
+
+        public MainViewModel()
+        {
+            _calculatePrimesCmd = new AsyncCommand<double>(OnCalculatePrimes);
+        }
+
+        private Task OnCalculatePrimes(CancellationToken t, double primeCount)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var primes = GeneratePrimesNaive((int)primeCount);
+                Console.WriteLine($"Calculated {primes.Count} primes");
+            });
+        }
+
+
+        private static List<int> GeneratePrimesNaive(int n)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes.Count < n)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+            return primes;
+        }
+    }
+}
---
+++ Tcoc.Blog.Async/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Tcoc.Blog.Async
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Nur Zahlen zulassen 
+        private void PrimeCountTBPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+                e.Handled = true;
+        }
+    }
+}
